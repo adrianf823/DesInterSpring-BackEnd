4f784140dd5ee60a632de363271b18c842eca1d6
adrianf823 <adrianf823@gmail.com>
2020-02-24 21:39:48 +0100

    o

--- src/main/java/com/example/DesInterSpring/controller/UsuarioController.java
+++ src/main/java/com/example/DesInterSpring/controller/UsuarioController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,7 +26,7 @@ import com.example.DesInterSpring.services.UsuarioService;
 
 @RestController
 @RequestMapping("api/Usuarios")
-//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UsuarioController {
 
 	@Autowired
